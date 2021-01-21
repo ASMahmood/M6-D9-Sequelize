0ad450e0680b2fe142fdaab4147e8cc8ae5ec1c5
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-21 15:52:06 +0000

    posting authors works!

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const database = require("./database");
 const cors = require("cors");
 
+const servicesRoute = require("./services");
+
 const server = express();
 const port = process.env.PORT || 8008;
 
 server.use(cors());
 server.use(express.json());
+
+server.use("/api", servicesRoute);
 
 database.sequelize.sync({ force: false }).then((result) => {
   server.listen(port, () => {
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,59 +1,15 @@
 const express = require("express");
-const Model = require("../../utilities/model");
-const Authors = new Model("authors");
+const Author = require("../../database").Author; //BECAUSE DATABASE/INDEX.JS IS EXPORTING A MODELS OBJECT, WE CAN CALL THE AUTHOR MODEL STRAIGHT FROM THIS OBJECT
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  try {
-    const singleAuthor = await Authors.findById(req.params.id);
-    res.send(singleAuthor);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedAuthor = await Authors.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    );
-    res.send(updatedAuthor);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
-
 router.post("/", async (req, res) => {
   try {
-    const newAuthor = await Authors.save(req.body);
-    res.send(newAuthor);
+    const newAuthor = await Author.create(req.body); //.create IS A SEQUELIZE METHOD DOR MODELS, IT CREATES A NEW ROW IN THE TABLE
+    res.status(201).send(newAuthor);
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    await Authors.findByIdAndDelete(req.params.id);
-    res.send("EXECUTED");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
-
-router.get("/", async (req, res) => {
-  try {
-    const allAuthors = await Authors.findOne(req.query);
-    res.send(allAuthors);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+    res.status(500).send("Uh oh, something broke :(");
   }
 });
 
